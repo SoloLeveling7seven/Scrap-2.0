a6d00dc5dc669f48999eb6092e67e40662e53f1e
Solo Leveling <109235836+SoloLeveling7seven@users.noreply.github.com>
2024-06-20 22:26:49 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -46,7 +46,8 @@ async def scrap_(client: Client, message: Message):
             print("current -->",i)
             old_post = await ubot.get_messages(chat_id = channel_id , message_ids = i)
             if old_post != None:
-                await ubot.copy_media_group(chat_id = int(-1002183336442), from_chat_id = old_post.chat.id, message_id = i)
+                await old_post.copy(int(-1002183336442)
+                # await ubot.copy_media_group(chat_id = int(-1002183336442), from_chat_id = old_post.chat.id, message_id = i)
             pos_msg = old_post
             print(old_post.caption)
             old_post = old_post.caption
@@ -73,7 +74,7 @@ async def scrap_(client: Client, message: Message):
             print(e)
             pass
                    
-@ubot.on_message(filters.video & filters.private)
+@ubot.on_message((filters.video | filter.photo) & filters.private)
 async def down_(client: Client, message: Message):   
     global Loop
     global post_link
@@ -88,10 +89,10 @@ async def down_(client: Client, message: Message):
                 except Exception as e:
                     thumb = None
                 post_message = await client.send_video(chat_id = int(-1002183336442), video = file,thumb = thumb)     
-            # elif messsage.photo:
-            #     print("p")
-            #     file = await client.download_media(message , file_name = f"{message.id}.jpg")
-            #     post_message = await client.send_photo(chat_id = int(-1002183336442), photo = file)     
+            elif messsage.photo:
+                print("p")
+                file = await client.download_media(message , file_name = f"{message.id}.jpg")
+                post_message = await client.send_photo(chat_id = int(-1002183336442), photo = file)     
             os.remove(file)
             os.remove(thumb)  
             Loop = False
